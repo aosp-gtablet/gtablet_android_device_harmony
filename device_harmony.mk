468c8dc3237d3635f4b23e738a57ff5da6e0fc3a
jason parker <kornyone@gmail.com>
2011-03-10 23:15:03 -0600

    Revert "merging gojimi framework overlay"
    This reverts commit 010db823c5c4dee02b290da2e119dcd74dbbfea5.

--- device_harmony.mk
+++ device_harmony.mk
@@ -39,9 +39,9 @@ PRODUCT_PACKAGES += \
     overlay.tegra \
     lights.tegra \
     sensors.tegra \
+    audiofix.harmony \
     libreference-ril \
-    libreference-cdma-sms \
-    audiofix
+    libreference-cdma-sms
 
 # Use MDPI artwork
 PRODUCT_LOCALES += mdpi
